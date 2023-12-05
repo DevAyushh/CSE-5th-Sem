375a2addc12de86f5f644fdb3d0410c683eed66b
Ayush Dev <128251051+DevAyushh@users.noreply.github.com>
2023-12-05 20:19:02 +0530

    Delete OS Lab directory

--- OS Lab/1e.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-n=$1
-log=`echo "scale=2;l($n)/l(10)"|bc -l`
-echo $log
--- OS Lab/2c.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter the username: "
-read name
-
-if grep -c $name /etc/passwd
-then
-	echo "Valid User"
-else
-	echo "Invalid User"
-fi
